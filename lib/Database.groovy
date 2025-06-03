2401d4a18f7e3a833e7336217949ad639810167b
HobnobMancer <emma.e.hobbs@outlook.com>
2025-06-03 12:00:56 +0100

    add retrieving max upi

--- lib/Database.groovy
+++ lib/Database.groovy
@@ -26,4 +26,26 @@ class Database {
             this.sql.close()
         }
     }
+
+    String getMaxUPI() { // UPI of the most recent seq in UniParc
+        def query = "SELECT MAX(UPI) FROM UNIPARC.PROTEIN"
+        return this.sql.rows(query)[0][0]
+    }
+
+    void wipeProteinTable() {
+        """
+        CREATE TABLE UNIPARC.PROTEIN
+        (
+            ID NUMBER(15) NOT NULL,
+            UPI CHAR(13) NOT NULL,
+            TIMESTAMP DATE NOT NULL,
+            USERSTAMP VARCHAR2(30) NOT NULL,
+            CRC64 CHAR(16) NOT NULL,
+            LEN NUMBER(6) NOT NULL,
+            SEQ_SHORT VARCHAR2(4000),
+            SEQ_LONG CLOB,
+            MD5 VARCHAR2(32) NOT NULL
+        ) NOLOGGING
+        """
+    }
 }
